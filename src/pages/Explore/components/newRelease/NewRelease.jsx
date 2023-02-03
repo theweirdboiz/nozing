6accf76a6107d60a453ff096f7dbce95ce64e0f4
theweirdboiz <19130227@st.hcmuaf.edu.vn>
2023-02-03 16:46:02 +0700

    update home page

--- src/pages/Explore/components/newRelease/NewRelease.jsx
+++ src/pages/Explore/components/newRelease/NewRelease.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { homeDataSelector } from "@redux/selectors";
@@ -8,24 +8,31 @@ import SongItem from "../songItem";
 const NewRelease = (props) => {
   const { newRelease } = useSelector(homeDataSelector);
 
-  const formatReleaseDate = () => {};
-
+  const formatReleaseDate = useCallback((time) => {
+    const milis = Math.floor(Date.now() / 1000 - time);
+    const hour = Math.floor(milis / 3600);
+    const result = Math.floor(hour / 24);
+    if (result < 1) {
+      return "Hôm nay";
+    }
+    return `${result} ngày trước`;
+  }, []);
   return (
     <div className="mt-[4.8rem]">
       <h3 className="mb-[2rem] text-[2rem] font-bold">{newRelease?.title}</h3>
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-x-4 uppercase text-[1.2rem] font-medium mb-[1.6rem]">
-          <div className="bg-purple-primary rounded-3xl border border-alpha-bg px-[2.4rem] py-1">
+          <div className="bg-purple-primary rounded-3xl border border-alpha-bg px-[2.4rem] py-1 cursor-pointer">
             Tất cả
           </div>
-          <div className=" rounded-3xl border border-alpha-bg px-[2.4rem] py-1">
+          <div className=" rounded-3xl border border-alpha-bg px-[2.4rem] py-1 cursor-pointer">
             Việt Nam
           </div>
-          <div className=" rounded-3xl border border-alpha-bg px-[2.4rem] py-1">
+          <div className=" rounded-3xl border border-alpha-bg px-[2.4rem] py-1 cursor-pointer">
             Quốc tế
           </div>
         </div>
-        <div className="flex items-center gap-x-2">
+        <button className="flex items-center gap-x-2 text-secondary hover:text-link-text-hover uppercase font-semibold text-[1.2rem]">
           <span>Tất cả</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -41,11 +48,17 @@ const NewRelease = (props) => {
               d="M8.25 4.5l7.5 7.5-7.5 7.5"
             />
           </svg>
-        </div>
+        </button>
       </div>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-3 gap-x-8">
         {newRelease?.items?.all?.map((item) => {
-          return <SongItem key={item?.encodeId} item={item} />;
+          return (
+            <SongItem
+              key={item?.encodeId}
+              item={item}
+              date={formatReleaseDate}
+            />
+          );
         })}
       </div>
     </div>
--- src/pages/Explore/components/songItem/SongItem.jsx
+++ src/pages/Explore/components/songItem/SongItem.jsx
@@ -2,18 +2,63 @@ import React, { memo } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const SongItem = ({ item }) => {
+const SongItem = ({ item, date }) => {
   return (
-    <div className="flex items-center gap-x-5 p-[1rem]">
-      <figure className="flex-shrink-0 rounded-lg overflow-hidden w-[6rem] h-[6rem]">
-        <img src={item?.thumbnail} alt="" />
-      </figure>
-      <div className="text-[1.2rem] font-medium text-secondary">
-        <h3 className="text-[1.4rem] text-white">{item?.title}</h3>
-        {item?.artists?.map((artist) => {
-          return <Link key={artist?.id}>{artist?.name} </Link>;
-        })}
-        <h4>{item?.releaseDate}</h4>
+    <div className="flex justify-between items-center relative group/item hover:bg-alpha-bg rounded-lg">
+      <div className="flex items-center gap-x-5 p-[1rem]">
+        <figure className="flex-shrink-0 rounded-lg overflow-hidden w-[6rem] h-[6rem] relative cursor-pointer">
+          <img src={item?.thumbnail} alt="" />
+          <div className="group-hover/item:flex hidden items-center justify-center absolute inset-0 w-full h-full z-20">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#fff"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-10 h-10"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+              />
+            </svg>
+          </div>
+          <div className="group-hover/item:block hidden absolute inset-0 w-full h-full bg-layout-bg opacity-40"></div>
+        </figure>
+        <div className="text-[1.2rem] font-medium text-secondary">
+          <h3 className="text-[1.4rem] text-white">{item?.title}</h3>
+          {item?.artists?.map((artist) => {
+            return (
+              <Link
+                className="hover:text-link-text-hover hover:underline"
+                key={artist?.id}
+              >
+                {artist?.name}{" "}
+              </Link>
+            );
+          })}
+          <h4>{date(item?.releaseDate)}</h4>
+        </div>
+        {/* <div className="absolute inset-0 w-full h-full bg-alpha-bg rounded-lg"></div> */}
+      </div>
+      <div className="group-hover/item:block hidden">
+        <button className="circle mr-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-10 h-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
