2860f32d3096d285397828f41ed1b868b04a11d7
theweirdboiz <19130227@st.hcmuaf.edu.vn>
2023-02-06 19:10:31 +0700

    fix not update queue

--- src/layouts/components/Queue/Queue.jsx
+++ src/layouts/components/Queue/Queue.jsx
@@ -42,7 +42,7 @@ const Queue = () => {
     if (currentPlaylistId && isPlaying) {
       setQueue(detailPlaylist);
     }
-  }, [currentPlaylistId]);
+  }, [currentPlaylistId, isPlaying]);
 
   useEffect(() => {
     isPlaying && setTabActive(0);
